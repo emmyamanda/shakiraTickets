796e1b572946507daf8b3fc1003cae368cb66c51
emmyamanda <emmysignal95@hotmail.com>
2022-06-08 14:16:26 +0100

    Basic index template for new gigs path

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,14 @@
+# from django.http import HttpResponse
+
+
+# def detail(request, gig_name):
+#     return HttpResponse("You're looking at gig %s." % gig_name)
+
 from django.http import HttpResponse
+from django.template import loader
 
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+def index(request):
+    template = loader.get_template('index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
